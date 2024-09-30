92b642ad7d69fb653aa93f140d8832b2a621f4c4
Federico Moretti <hello@federicomoretti.it>
2024-09-30 17:55:07 +0200

    first commit

---
+++ src/cs50p_package_fedtti/cs50p.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+import discord
+from openai import OpenAI
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello') or message.content.startswith('hi'):
+        await message.channel.send('hello, world')
+
+    if client.user.mentioned_in(message):
+        # TODO: @fedtti - Start a conversation with GPT-4o.
+        await message.channel.send('you\'re welcome')
+
+
+client.run(DISCORD_TOKEN)
